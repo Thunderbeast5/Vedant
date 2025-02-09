5c261c04d1edcb9e067db056854e158c1b6a95b9
Thunderbeast5 <vedant.purkarr05@gmail.com>
2025-02-09 23:29:51 +0530

    Change

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+import 'react-responsive-carousel/styles/carousel.min.css'; 
 
 const testimonials = [
   {
